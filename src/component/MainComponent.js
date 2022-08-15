2cb577feaf92d85d27cdfe25ec623553605075e1
hoangnmFX15203 <hoangnmfx15203@funix.edu.vn>
2022-08-16 01:08:03 +0700

    LAB04_01

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -3,13 +3,18 @@ import Menu from '~/component/MenuComponent';
 import React, { Component } from 'react';
 import { DISHES } from './shared/dishes';
 
-class App extends Component {
+class Main extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             dishes: DISHES,
+            selectedDish: null,
         };
+    }
+
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId });
     }
 
     render() {
@@ -20,10 +25,10 @@ class App extends Component {
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
-                <Menu dishes={this.state.dishes} />
+                <Menu onClick={(dishId) => this.onDishSelect(dishId)} dishes={this.state.dishes} />
             </div>
         );
     }
 }
 
-export default App;
+export default Main;
